a681b848fd5d9de290c7969d4ec19caca88e8d00
nspp99b <chromepanthers@gmail.com>
2018-04-29 23:18:25 -0400

    adds migration and model for Strokes

---
+++ db/migrate/20180430031659_create_strokes.rb
@@ -0,0 +1,11 @@
+class CreateStrokes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :strokes do |t|
+      t.integer :value
+      t.integer :user_id
+      t.integer :performance_id
+
+      t.timestamps
+    end
+  end
+end
