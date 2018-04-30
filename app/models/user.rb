8ef97fee9758c5477c268acca2e66ba2478a0e61
nspp99b <chromepanthers@gmail.com>
2018-04-29 23:27:45 -0400

    adds User relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :strokes
+  has_many :performances, through: :strokes
+  validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
 end
