c6ef371de82d1d363898440459a0880e6ecd02dc
nspp99b <chromepanthers@gmail.com>
2018-04-29 23:58:36 -0400

    addes dependent destroy of performance strokes and mods length validations

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,5 +1,5 @@
 class Performance < ApplicationRecord
-  has_many :strokes
+  has_many :strokes, dependent: :destroy
   has_many :users, through: :strokes
-  validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :name, presence: true, uniqueness: true, length: { in: 6..20 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :strokes
   has_many :performances, through: :strokes
-  validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
 end
