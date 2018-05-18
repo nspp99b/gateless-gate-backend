bfa0ab4f96996bf46aff9b7e82ee87a7582ced20
nspp99b <chromepanthers@gmail.com>
2018-05-17 21:57:43 -0400

    adds private auth methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,60 @@
 class ApplicationController < ActionController::API
+  private
+
+  def issue_token(payload)
+    JWT.encode(payload, secret, algorithm)
+  end
+
+  def login_user(username, password)
+    user = User.find_by(username: username)
+    if user && user.authenticate(password)
+      user
+    else
+      raise AuthError
+    end
+  end
+
+  def authorize_user!
+    if !current_user.present?
+      render json: {error: 'No user id present'}
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: token_user_id)
+  end
+
+  def token_user_id
+    decoded_token.first['user_id']
+  end
+
+  def decoded_token
+    if token
+      begin
+        JWT.decode(token, secret, true, {algorithm: algorithm})
+      rescue JWT::DecodeError
+        return [{}]
+      end
+    else
+      [{}]
+    end
+  end
+
+  def token
+    request.headers['Authorization']
+  end
+
+  def secret
+    "opiweurioqwehiadlshgaklshvkadshfgopqetoqhoqwueroqwieuoqipuerq"
+  end
+
+  def algorithm
+    "HS256"
+  end
+end
+
+class AuthError < StandardError
+  def initialize(msg="No user or invalid password")
+    super
+  end
 end
