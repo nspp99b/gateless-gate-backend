351a97f51b1b55d7ca3ce1048da5a19793274895
nspp99b <chromepanthers@gmail.com>
2018-05-17 22:48:03 -0400

    adds performances controller

---
+++ app/controllers/api/v1/performances_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::PerformancesController < ApplicationController
+
+  def index
+    @performances = Performance.all
+    render json: @performances, status: 200
+  end
+
+  def create
+    @performance = Performance.new(performance_params)
+    if @performance.valid?
+      @performance.save
+      render json: @performance, status: 200
+    else
+      render json: {error: 'A room with this name already exists!'}
+    end
+  end
+
+  def show
+    @performance = Performance.find_by(id: params[:id])
+    render json: @performance, status: 200
+  end
+
+  private
+
+  def perfomance_params
+    params.require(:performance).permit(:name)
+  end
+
+end
